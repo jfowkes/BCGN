69a59f4b875655859ca4b47342872e870f6b55f9
Jari <jaroslav.fowkes@maths.ox.ac.uk>
2016-10-18 11:28:47 +0100

    Fix hard case, better sampling and plotting

--- BCGN.py
+++ BCGN.py
@@ -4,87 +4,129 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.linalg as linalg
 import math as ma
+import warnings
+import pickle
 import cutermgr
 
 """ Main function """
 def main():
 
-    # Fix RNG seed
-    np.random.seed(0)
+    # Load test function minimizers
+    #minimizers = {}
+    minimizers = pickle.load(open('minimizers.ser','rb'))
 
     # Main parameters
     TOL = 1e-15
     K_MAX = 100
 
     # Loop over test functions
-    funcs = ['PowellSingular', 'BroydenTridiagonal', 'Osborne1', 'Chebyquad', 'Osborne2', 'COOLHANS']
+    #funcs = ['PowellSingular', 'BroydenTridiagonal', 'Osborne1', 'Chebyquad', 'Osborne2', 'COOLHANS', 'AIRCRFTA']
+    funcs = ['Osborne1','COOLHANS']
     for func in funcs:
+
+        # Fix RNG seed
+        np.random.seed(0)
 
         # Get test function
         r, J, x0 = get_test_problem(func)
+
         n = x0.size
+        xopt = minimizers[func]
+
+        # Plotting
+        fig = plt.figure(funcs.index(func) + 1,figsize=(24,6))
+        ax1 = fig.add_subplot(1,3,1)
+        ax2 = fig.add_subplot(1,3,2)
+        ax3 = fig.add_subplot(1,3,3)
 
         # Run RBCGN
-        plt.figure(funcs.index(func)+1)
         print '====== ' + func + ' ======'
         legend = []
         for p in range(1,n+1):
-            legend += ['Block Size: ' + str(p)]
+            legend += ['Block Size ' + str(p)]
             print
             print '======',legend[p-1], '======'
-            RBCGN(r,J,x0,K_MAX,TOL,p)
-        plt.title('RBCGN - ' + func + ' function (' + str(n) + 'D)')
-        plt.xlabel('Iterations')
-        plt.ylabel('Residual Norm')
-        plt.legend(legend)
-        plt.grid()
-        #plt.savefig('figures/'+func+'')
+            RBCGN(r,J,x0,xopt,K_MAX,TOL,p,fig)
+
+        #minimizers[func] = xopt
+
+        # Plotting
+        plt.suptitle('RBCGN - ' + func + ' function (' + str(n) + 'D)',fontsize=13)
+        ax1.legend(legend)
+        ax1.set_xlabel('Iterations')
+        ax1.set_ylabel('Norm Residual')
+        ax1.grid(True)
+        ax2.set_xlabel('Iterations')
+        ax2.set_ylabel('Norm Gradient')
+        ax2.grid(True)
+        ax3.set_xlabel('Iterations')
+        ax3.set_ylabel('Norm Minimizer')
+        ax3.grid(True)
+        plt.gcf().set_tight_layout(True)
+        #plt.savefig('figures/'+func+'_WR')
         plt.show()
 
-    plt.show()
+    #pickle.dump(minimizers,open('minimizers.ser','wb'),protocol=-1)
+
 
 """ Random Block-Coordinate Gauss-Newton """
-def RBCGN(r, J, x, k_max, tol, p, alg='tr', redrawFailed=False, plotFailed=True, dynamicUpdate=False, gaussSouthwell=False):
+def RBCGN(r, J, x, xopt, k_max, tol, p, fig, algorithm='tr', redrawFailed=True, withoutReplacement=False, plotFailed=True, update='none', gaussSouthwell=False):
 
     # Full function and gradient
     def f(z): return 0.5 * np.dot(r(z), r(z))
     def gradf(z): return J(z).T.dot(r(z))
 
     # Plotting
-    hl, = plt.semilogy(0,f(x),linewidth=2)
+    ax1 = fig.add_subplot(1,3,1)
+    ax2 = fig.add_subplot(1,3,2)
+    ax3 = fig.add_subplot(1,3,3)
+    hl1, = ax1.semilogy(0,f(x),nonposy='clip',linewidth=2)
+    hl2, = ax2.semilogy(0,linalg.norm(gradf(x)),nonposy='clip',linewidth=2)
+    hl3, = ax3.semilogy(0,linalg.norm(x-xopt),nonposy='clip',linewidth=2)
 
     # Set initial trust region radius
     delta = None
-    if alg == 'tr':
+    if algorithm == 'tr':
         delta = linalg.norm(gradf(x))/10
 
     k = 0
     pk = 0
     n = x.size
     accepted = True
+    coords_drawn = np.empty(0,dtype=int)
     while f(x) > tol and k < k_max:
 
         # Evaluate full gradient for Gauss-Southwell
-        if gaussSouthwell:
-            ngradf = np.fabs(gradf(x))
+        # TODO: only do this when we're doing GS
+        ngradf = np.fabs(gradf(x))
         
         # Randomly select blocks
         if redrawFailed or accepted:
+            coords = np.arange(n)
+            ngradf_coords = ngradf
+            coords_undrawn = np.setdiff1d(np.arange(n), coords_drawn)
+            if withoutReplacement and coords_undrawn.size >= p:
+                coords = coords_undrawn
+                ngradf_coords[coords_drawn] = 0
+            else: # reset
+                coords_drawn = np.empty(0, dtype=int)
+
             if gaussSouthwell:
-                S = np.argpartition(ngradf, -p)[-p:]
+                S = np.argpartition(ngradf_coords, -p)[-p:]
             else:
-                S = np.random.permutation(np.arange(n))[0:p]
+                S = np.random.permutation(coords)[0:p]
             U_S = np.zeros((n,p))
             for j in range(0,p):
                 U_S[S[j],j] = 1
+            coords_drawn = np.append(coords_drawn,np.nonzero(U_S)[0])
 
         # Assemble block-reduced matrices
         J_S = J(x).dot(U_S)
         gradf_S = J_S.T.dot(r(x))
 
-        # Dynamic: increase block size
-        if dynamicUpdate and p < n and linalg.norm(gradf_S) < ma.sqrt(tol):
-            print 'Gradient small, increasing block size to: ', p+1
+        # Increase block size dynamically or monotonically
+        if p < n and update == 'monotonic' and k % 5 == 0 or update == 'dynamic' and linalg.norm(gradf_S) < ma.sqrt(tol):
+            print update, '- increasing block size to: ', p+1
             S = np.nonzero(U_S)[0]
             inds = np.setdiff1d(np.arange(n),S)
             if gaussSouthwell:
@@ -94,14 +136,15 @@ def RBCGN(r, J, x, k_max, tol, p, alg='tr', redrawFailed=False, plotFailed=True,
             U_ind = np.zeros((n,1))
             U_ind[ind,:] = 1
             U_S = np.hstack((U_S,U_ind))
+            coords_drawn = np.append(coords_drawn,ind)
             J_S = J(x).dot(U_S)
             gradf_S = J_S.T.dot(r(x))
             p += 1
 
         # Output
-        monitor(k, r, x, f, delta, alg, accepted, gradf, gradf_S)
+        monitor(k, r, x, f, delta, algorithm, accepted, gradf, gradf_S)
 
-        if alg == 'tr':
+        if algorithm == 'tr':
             x, delta, accepted = trust_region(f, x, U_S, J_S, gradf_S, delta)
         else:
             x, delta = line_search(f, x, U_S, J_S, gradf_S)
@@ -110,18 +153,20 @@ def RBCGN(r, J, x, k_max, tol, p, alg='tr', redrawFailed=False, plotFailed=True,
         # Plotting
         if plotFailed or accepted:
             pk += 1
-            update_line(hl,pk,f(x))
+            update_line(ax1,hl1,pk,f(x))
+            update_line(ax2,hl2,pk,linalg.norm(gradf(x)))
+            update_line(ax3,hl3,pk,linalg.norm(x-xopt))
 
     # Output
-    monitor(k, r, x, f, delta, alg, accepted, gradf)
+    monitor(k, r, x, f, delta, algorithm, accepted, gradf)
 
     return x
 
 """ Output Monitoring Information """
-def monitor(k, r, x, f, delta, alg, accepted, gradf, gradf_S=None):
+def monitor(k, r, x, f, delta, algorithm, accepted, gradf, gradf_S=None):
 
     print '++++ Iteration', k, '++++'
-    if alg == 'tr':
+    if algorithm == 'tr':
         print 'delta: %.2e' % delta
         if not accepted: print "Step Failed!"
     elif delta is not None:
@@ -198,15 +243,14 @@ def trust_region(f, x, U_S, J_S, gradf_S, delta):
     ETA1 = 0.25
     ETA2 = 0.75
     GAMMA1 = 0.5
-    GAMMA2 = 2
+    GAMMA2 = 2.
 
     # Trust Region subproblem parameters
-    MEPS = 1e-15
     LEPS = 1e-5
     KE = 0.01
 
     # Solve trust region subproblem
-    s_S = trs(J_S, gradf_S, delta, MEPS, LEPS, KE)
+    s_S = trs(J_S, gradf_S, delta, LEPS, KE)
 
     # Evaluate sufficient decrease
     Js_S = J_S.dot(s_S)
@@ -229,14 +273,14 @@ def trust_region(f, x, U_S, J_S, gradf_S, delta):
     return x, delta, accepted
 
 """ Trust Region Subproblem """
-def trs(J_S, gradf_S, delta, meps, leps, Ke):
+def trs(J_S, gradf_S, delta, leps, Ke):
     p = J_S.shape[1]
 
     # QR on J_S
     _, R_S = linalg.qr(J_S, mode='economic')
 
     # J_S'J_S full rank
-    if(ma.fabs(R_S.diagonal().prod()) > meps):
+    if(np.linalg.matrix_rank(J_S)==p):
 
         # Set lambda (for newton iteration)
         lamda = 0
@@ -288,9 +332,14 @@ def trs(J_S, gradf_S, delta, meps, leps, Ke):
 
 """ Return roots of quadratic equation """
 def quadeq(a, b, c):
-   x1 = (-b + ma.sqrt(b * b - 4 * a * c)) / (2 * a)
-   x2 = (-b - ma.sqrt(b * b - 4 * a * c)) / (2 * a)
-   return x1, x2
+    warnings.simplefilter("error", RuntimeWarning)
+    try:
+        x1 = (-b + ma.sqrt(b * b - 4 * a * c)) / (2 * a)
+        x2 = (-b - ma.sqrt(b * b - 4 * a * c)) / (2 * a)
+    except RuntimeWarning: # failed step: delta too large
+        x1 = 0
+        x2 = 0
+    return x1, x2
 
 
 """ Test Problem Selector """
@@ -316,11 +365,11 @@ def get_test_problem(name):
     return r, J, x0
 
 """ Real-time plotting """
-def update_line(hl, x_data, y_data):
+def update_line(ax, hl, x_data, y_data):
     hl.set_xdata(np.append(hl.get_xdata(), x_data))
     hl.set_ydata(np.append(hl.get_ydata(), y_data))
-    plt.gca().relim()
-    plt.gca().autoscale_view()
+    ax.relim()
+    ax.autoscale_view()
     plt.draw()
 
 main()
