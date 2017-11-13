Bugfix: handle overflow in residual entries (NumPy norm can handle infs)

BCGN.py
@@ -222,7 +222,7 @@ def RBCGN(r, J, x0, fxopt, it_max, ftol, p, fig, kappa, algorithm='tr', partitio
         # Loop tolerance
         Js_S = J_S.dot(s_S)
         Delta_m = -np.dot(gradf_S,s_S) -0.5*np.dot(Js_S,Js_S)
-        stopping_rule = -Delta_m + (1-kappa)/2*np.power(linalg.norm(rx),2) > 0
+        stopping_rule = -Delta_m + (1-kappa)/2*np.power(np.linalg.norm(rx),2) > 0
         #Jx_S = J_S.dot(x.dot(U_S))
         #stopping_rule = -Delta_m + np.dot(Js_S,Jx_S) + (sigma/2)*np.power(linalg.norm(s_S),2) > 0
 
@@ -263,7 +263,7 @@ def RBCGN(r, J, x0, fxopt, it_max, ftol, p, fig, kappa, algorithm='tr', partitio
             # Loop tolerance
             Js_S = J_S.dot(s_S)
             Delta_m = -np.dot(gradf_S,s_S) -0.5*np.dot(Js_S,Js_S)
-            stopping_rule = -Delta_m + (1-kappa)/2*np.power(linalg.norm(rx),2) > 0
+            stopping_rule = -Delta_m + (1-kappa)/2*np.power(np.linalg.norm(rx),2) > 0
             #Jx_S = J_S.dot(x.dot(U_S))
             #stopping_rule = -Delta_m + np.dot(Js_S,Jx_S) + (sigma/2)*np.power(linalg.norm(s_S),2) > 0
 