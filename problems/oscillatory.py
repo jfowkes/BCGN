cf7d9dd306a5c206145bdc5041a245da13c911c5
Jari <jaroslav.fowkes@maths.ox.ac.uk>
2019-10-02 14:48:52 +0100

    Fix CUTEst nonzero test set definitions

--- problems/oscillatory.py
+++ problems/oscillatory.py
@@ -1,9 +1,9 @@
 """ 2 MHG problems where BCGN oscillates """
 
-funcs = ['COOLHANS','Osborne1']
+funcs = ['COOLHANS','OSBORNE1']
 
 args = [None,None]
 
 dimen = [9,5]
 
-fxopts = [0,2.732445e-05] 
+fxopts = [0,2.732445e-05] # since we assume 0.5||r(x)||^2
